24bb735b9dc9320d375ad6467ab7e55ac026e43f
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-07-14 02:22:12 -0400

    added fight & battle_status methods to Battle

--- week3/classes/interracting-with-classes/ninjaBear/bear.rb
+++ week3/classes/interracting-with-classes/ninjaBear/bear.rb
@@ -12,8 +12,8 @@ end
 
 bear = Bear.new("Grizzly", 100, 12)
 ninja = Fighter.new('ninja', 100, 10)
-puts ninja.attack(bear)
-puts bear.attack(ninja)
-puts ninja.attack(bear)
-puts bear.inspect
-puts ninja.inspect
+# puts ninja.attack(bear)
+# puts bear.attack(ninja)
+# puts ninja.attack(bear)
+# puts bear.inspect
+# puts ninja.inspect
--- week3/classes/interracting-with-classes/ninjaBear/fighter.rb
+++ week3/classes/interracting-with-classes/ninjaBear/fighter.rb
@@ -21,7 +21,7 @@ end
 boxer1 = Fighter.new('Rocky', 110, 15)
 boxer2 = Fighter.new('Muhammad Ali', 100, 20)
 
-puts boxer1.attack(boxer2)                    #} all of this is to test that
-puts boxer2.inspect                           # Fighter class performs as we
-puts boxer2.attack(boxer1)                    # want
-puts boxer1.inspect
+# puts boxer1.attack(boxer2)                    #} all of this is to test that
+# puts boxer2.inspect                           # Fighter class performs as we
+# puts boxer2.attack(boxer1)                    # want
+# puts boxer1.inspect
